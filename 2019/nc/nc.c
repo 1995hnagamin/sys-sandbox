42aba863b6ac7337641f177c7550ed4a9c176fdb
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-18 20:34:20 +0900

    Introduce run_server() to 2019/nc/nc.c

--- 2019/nc/nc.c
+++ 2019/nc/nc.c
@@ -4,9 +4,14 @@
 #include <unistd.h>
 
 int
-main(int argc, char* argv[]) {
+run_server(int port) {
 	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	printf("listenfd = %d\n", listenfd);
 	close(listenfd);
 	return 0;
+}
+
+int
+main(int argc, char* argv[]) {
+	return run_server();
 }
