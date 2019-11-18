fd260e44715fa1d56daf0b98d5a0a5cbb0171de1
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-18 20:21:46 +0900

    Create a socket and close it immediately

--- 2019/nc/nc.c
+++ 2019/nc/nc.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int
 main(int argc, char* argv[]) {
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	printf("listenfd = %d\n", listenfd);
+	close(listenfd);
 	return 0;
 }
