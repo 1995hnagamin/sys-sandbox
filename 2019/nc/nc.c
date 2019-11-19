4bb3c02e77bdd79c52a626b026f810ef8abcac33
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-19 12:03:18 +0900

    Introduce readline_printf() to nc.c

--- 2019/nc/nc.c
+++ 2019/nc/nc.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <time.h>
+
+ssize_t
+readline_printf(int sockfd, char *buf, size_t sz) {
+	assert(sz > 0);
+	ssize_t len = 0;
+	ssize_t res;
+	while ((res = read(sockfd, buf, sz - 1)) >= 0) {
+		printf("%s", buf);
+		len += res;
+		if (strchr(buf, '\n')) {
+			return len;
+		}
+	}
+	return res;
+}
 
 // 2019-11-18 12:34:56
 // 1234567890123456789
@@ -52,8 +68,10 @@ run_server(int port) {
 	for (int i = 0; i < max_cnt; ++i) {
 		size_t const readbufsz = 10;
 		char readbuf[readbufsz];
-		read(connfd, readbuf, readbufsz);
-		printf("%s", readbuf);
+		ssize_t res = readline_printf(connfd, readbuf, readbufsz);
+		if (res < 0) {
+			break;
+		}
 	}
 
 	close(connfd);
