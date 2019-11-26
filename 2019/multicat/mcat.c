bccef27e94612eed0af7c01a74199078c04c6836
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-26 18:12:30 +0900

    Declare `FILENAME_LENGTH` in local scope

--- 2019/multicat/mcat.c
+++ 2019/multicat/mcat.c
@@ -13,13 +13,10 @@
 // 1234567890123456789
 size_t const DATETIME_BUFFER_LENGTH = 25;
 
-// 123.123.123.123-65535.txt
-// 1234567890123456789012345
-size_t const FILENAME_LENGTH = 30;
-
 void
 handle_connection(int connfd, struct sockaddr_in *cliaddr) {
 	size_t const ipaddrsz = 18;
+	size_t const FILENAME_LENGTH = 30; // 123.123.123.123-65535.txt
 
 	printf("%s:%d\n", inet_ntoa(cliaddr->sin_addr), ntohs(cliaddr->sin_port));
 
