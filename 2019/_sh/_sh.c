06c7cc5022fa54a3234e1dd9825335f757af2f94
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-12-12 15:21:38 +0900

    Read line and print

--- 2019/_sh/_sh.c
+++ 2019/_sh/_sh.c
@@ -1,7 +1,24 @@
+#include "chvec.h"
 #include <stdio.h>
+
+struct chvec *
+ussh_read_line(void) {
+	struct chvec *cv = chvec_create(10);
+	for (;;) {
+		char c = getchar();
+		if (c == EOF || c == '\n') {
+			chvec_push_back(cv, '\0');
+			break;
+		}
+		chvec_push_back(cv, c);
+	}
+	return cv;
+}
 
 int
 main(int argc, char* argv[]) {
-	printf("hello\n");
+	struct chvec *cv = ussh_read_line();
+	printf("%s\n", chvec_ptr(cv));
+	chvec_free(cv);
 	return 0;
 }
