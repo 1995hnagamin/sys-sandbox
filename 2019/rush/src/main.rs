25bec0ba80fa4d653d859a6f13d4109badccc0bc
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-14 22:14:09 +0900

    Read line and call execvp(3)

--- 2019/rush/src/main.rs
+++ 2019/rush/src/main.rs
@@ -1,3 +1,22 @@
+use std::ffi::CString;
+
+fn rush_repl() {
+    use std::io::{stdin, stdout, Write};
+    print!("$ ");
+    let _ = stdout().flush();
+
+    let mut line = String::new();
+    stdin().read_line(&mut line).expect("could not read line");
+    line.pop();
+    let line = CString::new(line).expect("could not create CString");
+
+    let res = nix::unistd::execvp(&line, &[&line.clone()]);
+    let res = match res {
+        Ok(_) => 1,
+        Err(error) => panic!("{:?}", error),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    rush_repl();
 }
