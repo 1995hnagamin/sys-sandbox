c06bc52031c31a088dbac15f3164fc92579894e5
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-15 19:45:24 +0900

    Call fork(2) before execvp(3)

--- 2019/rush/src/main.rs
+++ 2019/rush/src/main.rs
@@ -1,20 +1,27 @@
 use std::ffi::CString;
 
 fn rush_repl() {
+    use nix::unistd::ForkResult;
     use std::io::{stdin, stdout, Write};
-    print!("$ ");
-    let _ = stdout().flush();
+    loop {
+        print!("$ ");
+        let _ = stdout().flush();
 
-    let mut line = String::new();
-    stdin().read_line(&mut line).expect("could not read line");
-    line.pop();
-    let line = CString::new(line).expect("could not create CString");
+        let mut line = String::new();
+        stdin().read_line(&mut line).expect("could not read line");
+        line.pop();
+        let line = CString::new(line).expect("could not create CString");
 
-    let res = nix::unistd::execvp(&line, &[&line.clone()]);
-    let res = match res {
-        Ok(_) => 1,
-        Err(error) => panic!("{:?}", error),
-    };
+        match nix::unistd::fork() {
+            Ok(ForkResult::Child) => {
+                let _ = nix::unistd::execvp(&line, &[&line.clone()]).expect("execvp error");
+            }
+            Ok(ForkResult::Parent { child: _ }) => {
+                let _ = nix::sys::wait::wait();
+            }
+            Err(_) => panic!("fork error"),
+        }
+    }
 }
 
 fn main() {
