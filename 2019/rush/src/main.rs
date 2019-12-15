d94f7b8a604da4e13e64649a2732abd517862a9c
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-15 21:38:32 +0900

    Split rush_repl() into two functions

--- 2019/rush/src/main.rs
+++ 2019/rush/src/main.rs
@@ -1,37 +1,41 @@
 use std::ffi::CString;
 mod parse;
 
-fn rush_repl() {
+fn rush_read_eval_print() {
     use nix::unistd::ForkResult;
     use std::io::{stdin, stdout, Write};
-    loop {
-        print!("$ ");
-        let _ = stdout().flush();
+    print!("$ ");
+    let _ = stdout().flush();
 
-        let mut line = String::new();
-        stdin().read_line(&mut line).expect("could not read line");
-        line.pop();
-        let cmd = parse::parse(&line)
-            .and_then(|words| {
-                words
-                    .iter()
-                    .copied()
-                    .map(|word| CString::new(word))
-                    .collect::<Result<Vec<_>, _>>()
-                    .map_err(|err| err.to_string())
-            })
-            .unwrap();
+    let mut line = String::new();
+    stdin().read_line(&mut line).expect("could not read line");
+    line.pop();
+    let cmd = parse::parse(&line)
+        .and_then(|words| {
+            words
+                .iter()
+                .copied()
+                .map(|word| CString::new(word))
+                .collect::<Result<Vec<_>, _>>()
+                .map_err(|err| err.to_string())
+        })
+        .unwrap();
 
-        match nix::unistd::fork() {
-            Ok(ForkResult::Child) => {
-                let cmd: Vec<_> = cmd.iter().map(|word| word.as_c_str()).collect();
-                let _ = nix::unistd::execvp(cmd[0], &cmd).expect("execvp error");
-            }
-            Ok(ForkResult::Parent { child: _ }) => {
-                let _ = nix::sys::wait::wait();
-            }
-            Err(_) => panic!("fork error"),
+    match nix::unistd::fork() {
+        Ok(ForkResult::Child) => {
+            let cmd: Vec<_> = cmd.iter().map(|word| word.as_c_str()).collect();
+            let _ = nix::unistd::execvp(cmd[0], &cmd).expect("execvp error");
         }
+        Ok(ForkResult::Parent { child: _ }) => {
+            let _ = nix::sys::wait::wait();
+        }
+        Err(_) => panic!("fork error"),
+    }
+}
+
+fn rush_repl() {
+    loop {
+        rush_read_eval_print();
     }
 }
 
