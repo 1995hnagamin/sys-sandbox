27247103c8543bb7552f400c9c9220388b5f1946
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-15 22:05:09 +0900

    Use `?` in rush_read_eval_print()

--- 2019/rush/src/main.rs
+++ 2019/rush/src/main.rs
@@ -1,41 +1,49 @@
+use std::error::Error;
 use std::ffi::CString;
 mod parse;
 
-fn rush_read_eval_print() {
+fn rush_read_eval_print() -> Result<(), Box<Error>> {
     use nix::unistd::ForkResult;
     use std::io::{stdin, stdout, Write};
     print!("$ ");
-    let _ = stdout().flush();
+    stdout().flush()?;
 
     let mut line = String::new();
-    stdin().read_line(&mut line).expect("could not read line");
-    line.pop();
-    let cmd = parse::parse(&line)
-        .and_then(|words| {
-            words
-                .iter()
-                .copied()
-                .map(|word| CString::new(word))
-                .collect::<Result<Vec<_>, _>>()
-                .map_err(|err| err.to_string())
-        })
-        .unwrap();
-
-    match nix::unistd::fork() {
-        Ok(ForkResult::Child) => {
-            let cmd: Vec<_> = cmd.iter().map(|word| word.as_c_str()).collect();
-            let _ = nix::unistd::execvp(cmd[0], &cmd).expect("execvp error");
-        }
-        Ok(ForkResult::Parent { child: _ }) => {
-            let _ = nix::sys::wait::wait();
-        }
-        Err(_) => panic!("fork error"),
+    if stdin().read_line(&mut line)? == 0 {
+        println!("exit");
+        std::process::exit(0);
     }
+    line.pop();
+    let cmd = parse::parse(&line).and_then(|words| {
+        words
+            .iter()
+            .copied()
+            .map(|word| CString::new(word))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|err| err.to_string())
+    })?;
+    if cmd.len() < 1 {
+        return Ok(());
+    }
+
+    match nix::unistd::fork()? {
+        ForkResult::Child => {
+            let cmd: Vec<_> = cmd.iter().map(|word| word.as_c_str()).collect();
+            nix::unistd::execvp(cmd[0], &cmd)?;
+        }
+        ForkResult::Parent { child: _ } => {
+            nix::sys::wait::wait()?;
+        }
+    };
+    Ok(())
 }
 
 fn rush_repl() {
     loop {
-        rush_read_eval_print();
+        match rush_read_eval_print() {
+            Ok(()) => (),
+            Err(err) => println!("rush: {}", err.to_string()),
+        }
     }
 }
 
