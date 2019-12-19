f2687cbd61c3a4d3001f8b95652484ab392c2fc9
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-20 01:10:30 +0900

    Add run_server() to main.rs

--- 2019/rsnc/src/main.rs
+++ 2019/rsnc/src/main.rs
@@ -1,3 +1,4 @@
+use nix::sys::socket::*;
 use std::env;
 use std::error::Error;
 
@@ -10,4 +11,25 @@ fn main() {
     let portnum: u16 = args[1].parse().expect("not a number");
     println!("{}", portnum);
     run_server(portnum).unwrap();
+}
+
+fn run_server(portnum: u16) -> Result<(), Box<Error>> {
+    let listenfd = socket(
+        AddressFamily::Inet,
+        SockType::Stream,
+        SockFlag::empty(),
+        SockProtocol::Tcp,
+    )?;
+
+    let ip_addr = IpAddr::new_v4(127, 0, 0, 1);
+    let addr = SockAddr::new_inet(InetAddr::new(ip_addr, portnum));
+    bind(listenfd, &addr)?;
+
+    let backlog = 5;
+    listen(listenfd, backlog)?;
+
+    loop {
+        let connfd = accept(listenfd);
+        handle_connection(connfd);
+    }
 }
