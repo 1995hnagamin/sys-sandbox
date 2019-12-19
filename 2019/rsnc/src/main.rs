75eb9f910bb134df5e732631bd0bf5c96a23d76e
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-20 03:07:06 +0900

    Introduce src/sockbuf.rs

--- 2019/rsnc/src/main.rs
+++ 2019/rsnc/src/main.rs
@@ -1,11 +1,11 @@
 use nix::sys::select;
-use nix::sys::select::FdSet;
 use nix::sys::socket::*;
 use nix::unistd;
 use std::env;
 use std::error::Error;
 use std::os::unix::io::RawFd;
 use std::vec::Vec;
+mod sockbuf;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -40,81 +40,24 @@ fn run_server(portnum: u16) -> Result<(), Box<Error>> {
     Ok(())
 }
 
-struct SockBuf {
-    infd: RawFd,
-    outfd: RawFd,
-    buf: Vec<u8>,
-    cur: usize,
-    end: usize,
-}
-
-impl SockBuf {
-    fn new(infd: RawFd, outfd: RawFd, bufsz: usize) -> SockBuf {
-        SockBuf {
-            infd,
-            outfd,
-            buf: vec![0; bufsz],
-            cur: 0,
-            end: 0,
-        }
-    }
-    fn empty(&self) -> bool {
-        self.cur == self.end
-    }
-    fn read(&mut self) -> nix::Result<usize> {
-        self.cur = 0;
-        let result = unistd::read(self.infd, &mut self.buf);
-        match result {
-            Ok(size) => {
-                self.end = size;
-                result
-            }
-            Err(_) => result,
-        }
-    }
-    fn write(&mut self) -> nix::Result<usize> {
-        let result = unistd::write(self.outfd, &self.buf[0..self.end]);
-        match result {
-            Ok(size) => {
-                self.cur += size;
-                result
-            }
-            Err(_) => result,
-        }
-    }
-}
-
-fn make_rwset(sockbufs: &[SockBuf]) -> (FdSet, FdSet) {
-    let mut rset = FdSet::new();
-    let mut wset = FdSet::new();
-    for sb in sockbufs {
-        if sb.empty() {
-            rset.insert(sb.infd)
-        } else {
-            wset.insert(sb.outfd)
-        }
-    }
-    (rset, wset)
-}
-
 fn handle_connection(connfd: RawFd) -> Result<(), Box<Error>> {
     let maxfdp1 = 1 + connfd;
     let buf_len = 10;
     let mut sbs = [
-        SockBuf::new(0, connfd, buf_len),
-        SockBuf::new(connfd, 1, buf_len),
+        sockbuf::SockBuf::new(0, connfd, buf_len),
+        sockbuf::SockBuf::new(connfd, 1, buf_len),
     ];
 
     loop {
-        let (mut rset, mut wset) = make_rwset(&sbs);
+        let (mut rset, mut wset) = sockbuf::make_rwset(&sbs);
         let _nfds = select::select(maxfdp1, &mut rset, &mut wset, None, None);
         for p in sbs.iter_mut() {
-            if rset.contains(p.infd) {
+            if rset.contains(p.infd()) {
                 if p.read()? == 0 {
                     return Ok(());
                 }
             }
-            if wset.contains(p.outfd) {
+            if wset.contains(p.outfd()) {
                 if p.write()? == 0 {
                     return Ok(());
                 }
---
+++ 2019/rsnc/src/sockbuf.rs
@@ -0,0 +1,66 @@
+use nix::sys::select::FdSet;
+use nix::unistd;
+use std::os::unix::io::RawFd;
+
+pub struct SockBuf {
+    infd: RawFd,
+    outfd: RawFd,
+    buf: Vec<u8>,
+    cur: usize,
+    end: usize,
+}
+
+impl SockBuf {
+    pub fn new(infd: RawFd, outfd: RawFd, bufsz: usize) -> SockBuf {
+        SockBuf {
+            infd,
+            outfd,
+            buf: vec![0; bufsz],
+            cur: 0,
+            end: 0,
+        }
+    }
+    pub fn infd(&self) -> RawFd {
+        self.infd
+    }
+    pub fn outfd(&self) -> RawFd {
+        self.outfd
+    }
+    pub fn empty(&self) -> bool {
+        self.cur == self.end
+    }
+    pub fn read(&mut self) -> nix::Result<usize> {
+        self.cur = 0;
+        let result = unistd::read(self.infd, &mut self.buf);
+        match result {
+            Ok(size) => {
+                self.end = size;
+                result
+            }
+            Err(_) => result,
+        }
+    }
+    pub fn write(&mut self) -> nix::Result<usize> {
+        let result = unistd::write(self.outfd, &self.buf[0..self.end]);
+        match result {
+            Ok(size) => {
+                self.cur += size;
+                result
+            }
+            Err(_) => result,
+        }
+    }
+}
+
+pub fn make_rwset(sockbufs: &[SockBuf]) -> (FdSet, FdSet) {
+    let mut rset = FdSet::new();
+    let mut wset = FdSet::new();
+    for sb in sockbufs {
+        if sb.empty() {
+            rset.insert(sb.infd)
+        } else {
+            wset.insert(sb.outfd)
+        }
+    }
+    (rset, wset)
+}
