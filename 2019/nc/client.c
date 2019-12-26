12d04d0135eae0b82a840d4c5184d2824796e81e
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-12-26 19:23:43 +0900

    s/hostname/address/

--- 2019/nc/client.c
+++ 2019/nc/client.c
@@ -9,17 +9,17 @@
 #include "sockbuf.h"
 
 int
-connect_server(char *hostname, int port) {
+connect_server(char *address, int port) {
 	int connfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in servaddr;
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(port);
-	int status = inet_pton(AF_INET, hostname, &servaddr.sin_addr);
+	int status = inet_pton(AF_INET, address, &servaddr.sin_addr);
 	switch (status) {
 		case 0:
-			fprintf(stderr, "invalid network address: \"%s\"\n", hostname);
+			fprintf(stderr, "invalid network address: \"%s\"\n", address);
 			return -1;
 		case -1:
 			fprintf(stderr, "error\n");
@@ -32,7 +32,7 @@ connect_server(char *hostname, int port) {
 int
 main(int argc, char *argv[]) {
 	if (argc < 3) {
-		printf("usage: client <hostname> <port>\n");
+		printf("usage: client <address> <port>\n");
 		return 0;
 	}
 	int port;
