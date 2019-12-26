74e3d9083f81a853ef6ddfdbd0f1e08fbd5357d9
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-12-26 23:22:50 +0900

    Introduce struct IcmpEchoMsg

--- 2019/rping/src/main.rs
+++ 2019/rping/src/main.rs
@@ -1,3 +1,14 @@
+extern crate pnet;
+
+struct IcmpEchoMsg {
+    icmp_type: u8,
+    code: u8,
+    checksum: u16,
+    ident: u16,
+    seqno: u16,
+    data: Vec<u8>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
