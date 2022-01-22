03c936522367de9af1b036ed87cf4dd7e2a5eb84
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2022-01-22 13:50:54 +0900

    Add 2022/hills/

---
+++ 2022/hills/hills.cpp
@@ -0,0 +1,55 @@
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+
+size_t constexpr Width = 5;
+size_t constexpr Base = 27;
+std::string const Digits = "abcdefghijklmnopqrstuvwxyz*";
+
+int from_chars(std::string_view sv) {
+  int val = 0;
+  for (char c : sv) {
+    val *= Base;
+    val += (c == '*') ? (Base-1) : (c-'a');
+  }
+  return val;
+}
+
+std::string to_chars(int val) {
+  std::array<char, Width+1> arr;
+  for (int i = Width-1; i >= 0; --i) {
+    arr[i] = Digits[val % Base];
+    val /= Base;
+  }
+  arr[Width] = '\0';
+  return arr.data();
+}
+
+size_t constexpr SIZE = 14348907; // (26+1)^5
+int count[SIZE];
+
+int main() {
+  std::string line;
+  while (std::cin >> line) {
+    for (int i = 0; i < Width; ++i) {
+      std::string s = line;
+      s[i] = '*';
+      count[from_chars(s)] += 1;
+    }
+  }
+
+  int const threshold = 7;
+  std::vector<int> dist(20, 0);
+  for (int val = 0; val < SIZE; ++val) {
+    dist[count[val]] += 1;
+    if (count[val] >= threshold) {
+      std::cout << to_chars(val) << "\t" << count[val] << "\n";
+    }
+  }
+  for (int i = 2; i < 20; ++i) {
+    std::cout << i << "\t" << dist[i] << "\n";
+  }
+
+  return 0;
+}
