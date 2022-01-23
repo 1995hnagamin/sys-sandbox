ca77dab44a89b0cded29ecf9abbd46007afae426
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2022-01-23 17:02:36 +0900

    Update tab.pl

--- 2022/hills/tab.pl
+++ 2022/hills/tab.pl
@@ -8,10 +8,20 @@ my @dict = sort <IN>;
 chomp @dict;
 close IN;
 
+my $total = 0;
+my %uniq = ();
+
 print "<ul>\n";
 while (<>) {
 	my @cols = split /\t/, $_;
 	my $regex = ($cols[0] =~ s/\*/./r);
-	print "  <li>($cols[0])  ", join(', ', grep { /$regex/ } @dict), "</li>\n";
+	my @words = grep { /$regex/ } @dict;
+	print "  <li>($cols[0])  ", join(', ', @words), "</li>\n";
+	$total += scalar(@words);
+	foreach (@words) {
+		$uniq{$_} = 1;
+	}
 }
 print "</ul>\n";
+print scalar(%uniq)."\n";
+print $total."\n";
