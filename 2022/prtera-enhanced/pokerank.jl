ff872b31e0671d2e20147824f4f3f2c2e9877e74
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2022-11-20 18:51:33 +0900

    Use Int instead of String

--- 2022/prtera-enhanced/pokerank.jl
+++ 2022/prtera-enhanced/pokerank.jl
@@ -23,21 +23,21 @@ import Statistics
     STL: はがね
     FRY: フェアリー
 =#
-types = [
+ALLTYPE_ABBREVS = [ # all types
     "---",
     "NRM", "FIR", "WTR", "ELC", "GRS",
     "ICE", "FGT", "PSN", "GRD", "FLY",
     "PSY", "BUG", "RCK", "GHT", "DRG",
     "DRK", "STL", "FRY"
 ]
-N_TYPES = length(types)
+NALLTYPES = length(ALLTYPE_ABBREVS)
 
-tyidx = Dict{String, Int}()
-for (i, type) in enumerate(types)
-    tyidx[type] = i
+ALTYIDX = Dict{String, Int}()
+for (i, type) in enumerate(ALLTYPE_ABBREVS)
+    ALTYIDX[type] = i
 end
 
-M = [
+TYPE_CHART = [
     #    N  F  W  E  G    I  F  P  G  F    P  B  R  G  D    D  S  F
     #    R  I  T  L  R    C  G  S  R  L    S  U  C  H  R    R  T  R
     #    M  R  R  C  S    E  T  N  D  Y    Y  G  K  T  G    K  L  Y
@@ -67,74 +67,82 @@ M = [
     1    1 .5  1  1  1    1  2 .5  1  1    1  1  1  1  2    2 .5  1     # FRY
 ]
 
+TYPE_ABBREVS = [
+    "---",
+    "WTR", "ELC", "GRD", "FLY"
+]
+NTYPES = length(TYPE_ABBREVS)
+
+M = Matrix{Float64}(undef, NTYPES, NTYPES)
+for i = 1:NTYPES
+    ti = ALTYIDX[TYPE_ABBREVS[i]]
+    for j = 1:NTYPES
+        tj = ALTYIDX[TYPE_ABBREVS[j]]
+        M[i, j] = TYPE_CHART[ti, tj]
+    end
+end
+
 # effectiveness of a move of type tm
 eff(tm, (ty1, ty2)) = M[tm, ty1] * M[tm, ty2]
 
-isvalid(ty) = (ty != 1)
+NULLT = 1
+isvalid(ty) = (ty != NULLT)
 
-teff(tm, (t1, t2, tx)) = isvalid(tx) ? eff(tm, (1, tx)) : eff(tm, (t1, t2))
+teff(tm, (t1, t2, tx)) = isvalid(tx) ? eff(tm, (NULLT, tx)) : eff(tm, (t1, t2))
 
 function maxeff((s1, s2, sx), (t1, t2, tx))
-    tty = isvalid(tx) ? (1, tx) : (t1, t2)
+    tty = isvalid(tx) ? (NULLT, tx) : (t1, t2)
     multiplier(s) = eff(s, tty) * (s == sx ? 2 : 1.5)
 
     max(multiplier(s1), multiplier(s2), eff(sx, tty)*1.5)
 end
 
+sz = NTYPES * (NTYPES-1) ÷ 2 * NTYPES
 
-types = [
-    "---",
-    "WTR", "ELC", "GRD", "FLY"
-]
-N_TYPES = length(types)
-
-
-ctyidx = Dict{Tuple{String, String, String}, Int}()
-ctyrev = Dict{Int, Tuple{String, String, String}}()
-for (i, ((t1, t2), tx)) in enumerate(IterTools.product(IterTools.subsets(types, Val{2}()), types))
-    ctyidx[t1, t2, tx] = i
-    ctyrev[i] = (t1, t2, tx)
+CTYIDX = Dict{Tuple{Int, Int, Int}, Int}()
+CTYREV = Vector{Tuple{Int,Int,Int}}(undef, sz)
+for (i, ((t1, t2), tx)) in enumerate(IterTools.product(IterTools.subsets(1:NTYPES, Val{2}()), 1:NTYPES))
+    CTYIDX[t1, t2, tx] = i
+    CTYREV[i] = (t1, t2, tx)
 end
 
-sz = N_TYPES * (N_TYPES-1) ÷ 2 * N_TYPES
 T = ones(sz, sz) / sz
 
-scores = ones(1, sz) / sz
+importance = ones(1, sz) / sz
 cezarom = ones(1, sz) / sz
-for iter = 1:1000
-    global scores, cezarom
-    expect = Dict{Tuple{String, String, String},Float64}()
-    for (t1, t2) in IterTools.subsets(types, Val{2}())
-        for s in types
-            ptot = scores[ctyidx[t1, t2, "---"]]
-            ex = eff(tyidx[s], (tyidx[t1], tyidx[t2]))
-            for tx in types
-                p = scores[ctyidx[t1, t2, tx]]
-                ptot += p
-                ex += p * eff(tyidx[s], (tyidx[tx], 1))
-            end
-            ex /= ptot
-            expect[s, t1, t2] = ex
+for iter = 1:100
+    global importance, cezarom
+    expect = Vector{Float64}(undef, sz)
+    for idx in 1:sz
+        (t1, t2, s) = CTYREV[idx]
+        ptot = importance[CTYIDX[t1, t2, NULLT]] # no terastal
+        ex = ptot * eff(s, (t1, t2))
+        for tx = 1:NTYPES
+            p = importance[CTYIDX[t1, t2, tx]]
+            ptot += p
+            ex += p * eff(s, (NULLT, tx)) # terastal tx
         end
+        ex /= ptot
+        expect[idx] = ex
     end
     function select((s1, s2, sx), (t1, t2))
         stab(s) = (s == sx ? 2 : 1.5)
         cands = sort([
-            (stab(s1)*expect[s1, t1, t2], s1),
-            (stab(s2)*expect[s2, t1, t2], s2),
-            (1.5*expect[sx, t1, t2], sx)
+            (stab(s1)*expect[CTYIDX[t1, t2, s1]], s1),
+            (stab(s2)*expect[CTYIDX[t1, t2, s2]], s2),
+            (1.5*expect[CTYIDX[t1, t2, sx]], sx)
         ], rev=true)
         return cands[1][2]
     end
     A = zeros(sz, sz)
     for j = 1:sz
-        (t1, t2, tx) = ctyrev[j]
+        tty = CTYREV[j]
+        (t1, t2, tx) = tty
         for i = 1:sz
-            (s1, s2, sx) = ctyrev[i]
-            tty = (tyidx[t1], tyidx[t2], tyidx[tx])
-            sty = (tyidx[s1], tyidx[s2], tyidx[sx])
-            wst = max(1.5/8, teff(tyidx[select((s1, s2, sx), (t1, t2))], tty))
-            wts = max(1.5/8, teff(tyidx[select((t1, t2, tx), (s1, s2))], sty))
+            sty = CTYREV[i]
+            (s1, s2, sx) = sty
+            wst = max(1.5/8, teff(select((s1, s2, sx), (t1, t2)), tty))
+            wts = max(1.5/8, teff(select((t1, t2, tx), (s1, s2)), sty))
             weight = log(2, wts/wst)
             if weight > 0
                 A[i, j] = weight
@@ -146,11 +154,11 @@ for iter = 1:1000
         S[i, :] = A[i, :]/sum(A[i,:])
     end
     G = 0.85*S + 0.15*T
-    new_scores = scores * G
-    new_cezarom = cezarom * iter / (iter + 1) + new_scores / (iter + 1)
+    new_importance = importance * G
+    new_cezarom = cezarom * iter / (iter + 1) + new_importance / (iter + 1)
     diff = LinearAlgebra.norm1(new_cezarom - cezarom)/sz
     println(diff)
-    scores = new_scores
+    importance = new_importance
     cezarom = new_cezarom
     #=
     result = sort([(scores[i], ctyrev[i]) for i in 1:sz], rev=true)
@@ -165,42 +173,40 @@ for iter = 1:1000
     =#
 end
 
-rank = cezarom / maximum(cezarom) * 100
-result = sort([(rank[i], ctyrev[i]) for i in 1:sz], rev=true)
+scores = cezarom / maximum(cezarom) * 100
+result = sort([(scores[i], CTYREV[i]) for i in 1:sz], rev=true)
 
 @printf("[Top 30]\n")
 for i = 1:30
     score, (t1, t2, tx) = result[i]
-    if t1 == "---"
+    if t1 == NULLT
         t1, t2 = t2, t1
     end
-    @printf("%02d: %s / %s (%s) ... %.10f\n", i, t1, t2, tx, score)
+    @printf("%02d: %s / %s (%s) ... %.10f\n", i, TYPE_ABBREVS[t1], TYPE_ABBREVS[t2], TYPE_ABBREVS[tx], score)
 end
 
 open("pokerank.csv", "w") do csv
-    @printf(csv, "# Type 1, Type 2, Tera Type, Score\n")
+    @printf(csv, "# Type 1, Type 2, Tera Type, Score, Importance\n")
     for row in result
         score, (t1, t2, tx) = row
-        if t1 == "---"
+        imp = cezarom[CTYIDX[t1, t2, tx]]
+        if t1 == NULLT
             t1, t2 = t2, t1
         end
-        @printf(csv, "%s,%s,%s,%.10f\n", t1, t2, tx, score)
+        @printf(csv, "%s,%s,%s,%.10f,%.10f\n", TYPE_ABBREVS[t1], TYPE_ABBREVS[t2], TYPE_ABBREVS[tx], imp, score)
     end
 end
 
-ttscore = Dict{String,Vector{Float64}}()
-for tx in types
-    ttscore[tx] = [
-        rank[ctyidx[t1, t2, tx]]
-        for (t1, t2) in IterTools.subsets(types, Val{2}())
-    ]
-end
+ttscore = [
+    [ scores[CTYIDX[t1, t2, tx]] for (t1, t2) in IterTools.subsets(1:NTYPES, Val{2}())]
+    for tx in 1:NTYPES
+]
 
-ttas = sort([(Statistics.mean(ttscore[tx]), tx) for tx in types], rev=true)
+ttas = sort([(Statistics.mean(ttscore[tx]), tx) for tx in 1:NTYPES], rev=true)
 
 @printf("\n[Tera Type average score]\n")
-for (score, ty) in sort(ttas,rev=true)
-    @printf("%s: %.10f\n", ty, score)
+for (score, ty) in ttas
+    @printf("%s: %.10f\n", TYPE_ABBREVS[ty], score)
 end
 
 open("ttas.csv", "w") do csv
@@ -212,6 +218,6 @@ open("ttas.csv", "w") do csv
         minm = minimum(ttscore[tx])
         maxm = maximum(ttscore[tx])
         @printf(csv, "%s,%.10f,%.10f,%.10f,%.10f,%.10f,%.10f,%.10f\n",
-            tx, mean,std,minm,qnt[1],qnt[2],qnt[3],maxm)
+            TYPE_ABBREVS[tx], mean,std,minm,qnt[1],qnt[2],qnt[3],maxm)
     end
 end
