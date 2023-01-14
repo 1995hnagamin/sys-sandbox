93af99bc82bb138d068516fb168d3ec77212543d
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2023-01-14 16:12:46 +0900

    Implement tokenizer

--- 2023/9cc/9cc.c
+++ 2023/9cc/9cc.c
@@ -1,5 +1,90 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+struct Token {
+  TokenKind kind;
+  struct Token *next;
+  int val;
+  char *str;
+};
+
+typedef struct Token Token;
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *CUR_TOKEN;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool consume(char op) {
+  if (CUR_TOKEN->kind != TK_RESERVED || CUR_TOKEN->str[0] != op) {
+    return false;
+  }
+  CUR_TOKEN = CUR_TOKEN->next;
+  return true;
+}
+
+void expect(char op) {
+  if (CUR_TOKEN->kind != TK_RESERVED || CUR_TOKEN-> str[0] != op) {
+    error("not equal to '%c'", op);
+  }
+  CUR_TOKEN = CUR_TOKEN->next;
+}
+
+int expect_number() {
+  if (CUR_TOKEN->kind != TK_NUM) {
+    error("not a number");
+  }
+  int val = CUR_TOKEN->val;
+  CUR_TOKEN = CUR_TOKEN->next;
+  return val;
+}
+
+bool at_eof() {
+  return CUR_TOKEN->kind == TK_EOF;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+  while (*p) {
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+    error("tokenization failed");
+  }
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -10,22 +95,16 @@ int main(int argc, char **argv) {
   printf(".intel_syntax noprefix\n");
   printf(".globl _main\n");
 
+  CUR_TOKEN = tokenize(argv[1]);
   printf("_main:\n");
-  char *p = argv[1];
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  printf("  mov rax, %d\n", expect_number());
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
       continue;
     }
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    fprintf(stderr, "unexpected character: '%c'\n", *p);
-    return 1;
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
   printf("  ret\n");
   return 0;
