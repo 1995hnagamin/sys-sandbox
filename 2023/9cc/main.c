3db01d9c6b62c5deb04a496c4debd2ff4547af1c
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2023-02-05 10:59:56 +0900

    Change function definition order

--- 2023/9cc/main.c
+++ 2023/9cc/main.c
@@ -6,6 +6,30 @@
 #include <string.h>
 #include "9cc.h"
 
+char *read_file(char *path);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "wrong number of arguments: expected 2, got %d\n", argc);
+    return 1;
+  }
+
+  FILENAME = argv[1];
+  INPUT_HEAD = read_file(FILENAME);
+  CUR_TOKEN = tokenize(INPUT_HEAD);
+  parse();
+
+  printf(".intel_syntax noprefix\n");
+  gen_str_lit();
+  printf(".globl _main\n");
+  for (int i = 0; code[i]; ++i) {
+    view_node(code[i]);
+    fprintf(stderr, "\n");
+    gen(code[i]);
+  }
+  return 0;
+}
+
 char *read_file(char *path) {
   FILE *fp = fopen(path, "r");
   if (!fp) {
@@ -29,29 +53,6 @@ char *read_file(char *path) {
   buf[size] = '\0';
   fclose(fp);
   return buf;
-}
-
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "wrong number of arguments: expected 2, got %d\n", argc);
-    return 1;
-  }
-
-
-  FILENAME = argv[1];
-  INPUT_HEAD = read_file(FILENAME);
-  CUR_TOKEN = tokenize(INPUT_HEAD);
-  parse();
-
-  printf(".intel_syntax noprefix\n");
-  gen_str_lit();
-  printf(".globl _main\n");
-  for (int i = 0; code[i]; ++i) {
-    view_node(code[i]);
-    fprintf(stderr, "\n");
-    gen(code[i]);
-  }
-  return 0;
 }
 
 void error(char *fmt, ...) {
