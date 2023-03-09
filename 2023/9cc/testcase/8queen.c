08f33389323eb0e5bf6118778828806162be7dc0
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2023-03-09 09:15:24 +0900

    Don't use `used[8]` in 8queen.c

--- 2023/9cc/testcase/8queen.c
+++ 2023/9cc/testcase/8queen.c
@@ -1,50 +1,40 @@
 int pos[8];
 
-int diag(int x, int y, int dir) {
-  int a;
-  a = x - 1;
-  int b;
-  b = y + dir;
-  while (a >= 0) {
-    if (pos[a] == b) {
+int diag(int i, int dir) {
+  int x;
+  x = i - 1;
+  int y;
+  y = pos[i] + dir;
+  while (x >= 0) {
+    if (pos[x] == y) {
       return 1;
     }
-    a = a - 1;
-    b = b + dir;
+    x = x - 1;
+    y = y + dir;
   }
   return 0;
 }
 
 int queen(int row) {
-  if (diag(row, pos[row], 1)) {
+  if (diag(row, 1)) {
     return 0;
   }
-  if (diag(row, pos[row], -1)) {
+  if (diag(row, 0)) {
+    return 0;
+  }
+  if (diag(row, -1)) {
     return 0;
   }
   if (row == 7) {
     return 1;
   }
-  int used[8];
   int i;
-  i = 0;
-  while (i < 8) {
-    used[i] = 0;
-    i = i + 1;
-  }
-  i = 0;
-  while (i <= row) {
-    used[pos[i]] = 1;
-    i = i + 1;
-  }
   int sum;
   sum = 0;
   i = 0;
   while (i < 8) {
-    if (used[i] == 0) {
-      pos[row + 1] = i;
-      sum = sum + queen(row + 1);
-    }
+    pos[row + 1] = i;
+    sum = sum + queen(row + 1);
     i = i + 1;
   }
   return sum;
@@ -60,5 +50,6 @@ int main() {
     sum = sum + queen(0);
     i = i + 1;
   }
+  printnum(sum);
   return sum;
 }
